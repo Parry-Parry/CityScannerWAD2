f96ca2e316f7028288a2d728311e08eddf69807f
David MacNeill <2381195M@student.gla.ac.uk>
2020-03-30 13:10:20 +0100

    Register page functionality complete

--- scanner/views.py
+++ scanner/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from scanner.forms import NightlifePageForm, LifestylePageForm, FoodAndDrinkPageForm
-from scanner.models import NightlifePage, LifestylePage, FoodAndDrinkPage
+from scanner.forms import NightlifePageForm, LifestylePageForm, FoodAndDrinkPageForm, UserForm, UserProfileForm
+from scanner.models import NightlifePage, LifestylePage, FoodAndDrinkPage, UserProfile
 
 # Create your views here.
 def add_nightlife_page(request):
@@ -38,3 +38,32 @@ def add_foodanddrink_page(request):
         else:
             print(form.errors)
     return render(request, 'scanner/add_foodanddrink_page.html', {'form':form})
+
+def register(request):
+    registered=False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile= profile_form.save(commit=False)
+            profile.user =user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+                    'scanner/register.html',
+                    context = {'user_form': user_form,
+                                'profile_form': profile_form,
+                                'registered': registered})
