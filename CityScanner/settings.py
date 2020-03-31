616cc4dbcb60e302ac85441a93fac71fbad79ead
Parry <2393265p@student.gla.ac.uk>
2020-03-31 18:21:01 +0100

    Added static dir
    still resolving static css isuue

--- CityScanner/settings.py
+++ CityScanner/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#Login
+STATIC_DIR = 'os.path.join(BASE_DIR, STATIC_URL)'
+# Login URL mapping
 LOGIN_URL='scanner:login'
