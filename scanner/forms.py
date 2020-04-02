0e362eea00714e19d36e3989823c2673c1901f30
Parry <2393265p@student.gla.ac.uk>
2020-04-02 20:44:41 +0100

    Updated regi form

--- scanner/forms.py
+++ scanner/forms.py
@@ -3,29 +3,30 @@ from scanner.models import Culture, NightlifePage, LifestylePage, FoodAndDrinkPa
 from django.contrib.auth.models import User
 from scanner.models import UserProfile
 
+
 class NightlifePageForm(forms.ModelForm):
     class Meta:
         model = NightlifePage
         fields = '__all__'
 
     name = forms.CharField(max_length=NightlifePage.NAME_MAX_LENGTH,
-                            help_text="Name of the location.")
-    culture = forms.ModelChoiceField(queryset=Culture.objects.all(),help_text="Culture")
+                           help_text="Name of the location.")
+    culture = forms.ModelChoiceField(queryset=Culture.objects.all(), help_text="Culture")
     business = forms.CharField(max_length=50,
-                help_text="What kind of business is this location")
+                               help_text="What kind of business is this location")
     url = forms.URLField(help_text="URL to the website")
 
-    street_num = forms.IntegerField(min_value=0,max_value=NightlifePage.STREET_NUM_MAX_LENGTH,
-                    help_text="Street number of building")
+    street_num = forms.IntegerField(min_value=0, max_value=NightlifePage.STREET_NUM_MAX_LENGTH,
+                                    help_text="Street number of building")
     post_code = forms.CharField(max_length=NightlifePage.POSTCODE_MAX_LENGTH,
-                help_text="Postcode")
-
+                                help_text="Postcode")
 
     short_desc = forms.CharField(max_length=NightlifePage.SHORT_DESC_MAX_LENGTH,
-                help_text="Provide a short description")
-    price_range = forms.IntegerField(min_value=0,max_value=100,
-                    help_text="On a scale of 0-100 how pricey is this location")
+                                 help_text="Provide a short description")
+    price_range = forms.IntegerField(min_value=0, max_value=100,
+                                     help_text="On a scale of 0-100 how pricey is this location")
     liscenced = forms.BooleanField(help_text="Is this location liscenced to sell alcohol")
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -35,28 +36,24 @@ class NightlifePageForm(forms.ModelForm):
         return cleaned_data
 
 
-
-
 class LifestylePageForm(forms.ModelForm):
-
     name = forms.CharField(max_length=LifestylePage.NAME_MAX_LENGTH,
-                            help_text="Name of the location.")
-    culture = forms.ModelChoiceField(queryset=Culture.objects.all(),help_text="Culture")
+                           help_text="Name of the location.")
+    culture = forms.ModelChoiceField(queryset=Culture.objects.all(), help_text="Culture")
 
     business = forms.CharField(max_length=50,
-                help_text="What kind of business is this location")
+                               help_text="What kind of business is this location")
     url = forms.URLField(help_text="URL to the website")
 
-    street_num = forms.IntegerField(min_value=0,max_value=LifestylePage.STREET_NUM_MAX_LENGTH,
-                    help_text="Street number of building")
+    street_num = forms.IntegerField(min_value=0, max_value=LifestylePage.STREET_NUM_MAX_LENGTH,
+                                    help_text="Street number of building")
     post_code = forms.CharField(max_length=LifestylePage.POSTCODE_MAX_LENGTH,
-                help_text="Postcode")
-
+                                help_text="Postcode")
 
     short_desc = forms.CharField(max_length=LifestylePage.SHORT_DESC_MAX_LENGTH,
-                help_text="Provide a short description")
-    price_range = forms.IntegerField(min_value=0,max_value=100,
-                    help_text="On a scale of 0-100 how pricey is this location")
+                                 help_text="Provide a short description")
+    price_range = forms.IntegerField(min_value=0, max_value=100,
+                                     help_text="On a scale of 0-100 how pricey is this location")
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -68,27 +65,26 @@ class LifestylePageForm(forms.ModelForm):
 
     class Meta:
         model = LifestylePage
-        fields='__all__'
+        fields = '__all__'
+
 
 class FoodAndDrinkPageForm(forms.ModelForm):
-
-    culture = forms.ModelChoiceField(queryset=Culture.objects.all(),help_text="Culture")
-
+    culture = forms.ModelChoiceField(queryset=Culture.objects.all(), help_text="Culture")
 
     name = forms.CharField(max_length=FoodAndDrinkPage.NAME_MAX_LENGTH,
-                            help_text="Name of the location.")
+                           help_text="Name of the location.")
 
     url = forms.URLField(help_text="URL to the website")
 
-    street_num = forms.IntegerField(min_value=0,max_value=FoodAndDrinkPage.STREET_NUM_MAX_LENGTH,
-                    help_text="Street number of building")
+    street_num = forms.IntegerField(min_value=0, max_value=FoodAndDrinkPage.STREET_NUM_MAX_LENGTH,
+                                    help_text="Street number of building")
     post_code = forms.CharField(max_length=FoodAndDrinkPage.POSTCODE_MAX_LENGTH,
-                help_text="Postcode")
+                                help_text="Postcode")
 
     short_desc = forms.CharField(max_length=FoodAndDrinkPage.SHORT_DESC_MAX_LENGTH,
-                help_text="Provide a short description")
-    price_range = forms.IntegerField(min_value=0,max_value=100,
-                    help_text="On a scale of 0-100 how pricey is this location")
+                                 help_text="Provide a short description")
+    price_range = forms.IntegerField(min_value=0, max_value=100,
+                                     help_text="On a scale of 0-100 how pricey is this location")
     vegan_option = forms.BooleanField(help_text="Does this restaurant have vegan options")
 
     def clean(self):
@@ -101,7 +97,7 @@ class FoodAndDrinkPageForm(forms.ModelForm):
 
     class Meta:
         model = FoodAndDrinkPage
-        fields='__all__'
+        fields = '__all__'
 
 
 class UserForm(forms.ModelForm):
@@ -109,7 +105,8 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields=('username', 'email', 'password')
+        fields = ('username', 'email', 'password')
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
